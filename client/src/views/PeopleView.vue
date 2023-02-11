4883bcdf0deefd91695e7554e2cd46916ee0e655
Gian.Nguyen <khonggian2007@gmail.com>
2023-02-11 13:13:39 +0700

    complete init fe vuejs

---
+++ client/src/views/PeopleView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+      </tr>
+      <tr v-for="person in people" :key="person.id">
+        <td>{{person.id}}</td>
+        <td>{{person.name}}</td>
+        <td>{{person.age}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data () {
+    return {
+      people: []
+    }
+  },
+  created () {
+    axios.get('/people').then(response => {
+      this.people = response.data;
+    });
+  }
+}
+</script>
