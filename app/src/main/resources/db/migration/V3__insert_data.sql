e042f4718d50cd81786dfccbc2a901162021eae3
Gian.Nguyen <khonggian2007@gmail.com>
2023-02-26 22:51:52 +0700

    update flyway password : 123456

--- app/src/main/resources/db/migration/V3__insert_data.sql
+++ app/src/main/resources/db/migration/V3__insert_data.sql
@@ -1,3 +1,6 @@
 INSERT INTO person (id, name, age) VALUES (1, 'Gian', 35);
 INSERT INTO person (id, name, age) VALUES (2, 'Nga', 29);
+INSERT INTO user_data (id, password, role, username) VALUES (2, '$2a$10$ycKiSgdWu0u1pKIHc9lUiOjPKkrRVKLYrZOUbCeTL5mZ26fjBrfSG', 'USER', 'nga.nguyen');
+INSERT INTO user_data (id, password, role, username) VALUES (1, '$2a$10$pm8Nubp0ij65BWYLq..lFeQYhramGBoYY4/E9I4KN.3UC0yQShCJS', 'ADMIN', 'gian.nguyen');
+
 
